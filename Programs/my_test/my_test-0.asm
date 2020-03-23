64527393b3049f692c2f03932cac6e03c2946db3
Tilen Marc <marct15@gmail.com>
2020-03-23 15:03:33 +0100

    Preset.

---
+++ Programs/my_test/my_test-0.asm
@@ -0,0 +1,17 @@
+# my_test-0--0
+ldsi s0, 1 # 0
+ldi c0, 2 # 1
+addm s1, s0, c0 # 2
+startopen 1, s1 # 3
+stopopen 1, c1 # 4
+print_char4 1701869669 # 5
+print_char4 1684370531 # 6
+print_char 32 # 7
+print_char 51 # 8
+print_char4 1869029420 # 9
+print_char 116 # 10
+print_char 32 # 11
+print_reg c1 # 12
+print_char 10 # 13
+reqbl 3 # 14
+reqbl 3 # 15
