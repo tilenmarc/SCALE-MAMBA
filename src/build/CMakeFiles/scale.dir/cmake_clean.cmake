625e04053b57343120acf72e334030cf66578a76
Tilen Marc <marct15@gmail.com>
2020-03-30 17:21:58 +0200

    Removing build.

--- src/build/CMakeFiles/scale.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libscale.a"
-  "libscale.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/scale.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
