4e207c08a02b7494613e4c7c4ea980b12535aa88
tilenmarc <marct15@gmail.com>
2020-07-08 12:00:54 +0200

    Update to new system.

---
+++ src/cmake-build-debug/CMakeFiles/local.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tilenmarc/git/SCALE_MAMBA_MY/src/Local/BLAS.cpp" "/home/tilenmarc/git/SCALE_MAMBA_MY/src/cmake-build-debug/CMakeFiles/local.dir/Local/BLAS.cpp.o"
+  "/home/tilenmarc/git/SCALE_MAMBA_MY/src/Local/Local_Functions.cpp" "/home/tilenmarc/git/SCALE_MAMBA_MY/src/cmake-build-debug/CMakeFiles/local.dir/Local/Local_Functions.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MAX_MOD_SZ=7"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
